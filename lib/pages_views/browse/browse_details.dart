e7ae2334ca687fe438d8de3928b017f5a354213a
amr <amrHDsaeed@github.com>
2024-03-30 09:36:09 +0200

    ++ handel

--- lib/pages_views/browse/browse_details.dart
+++ lib/pages_views/browse/browse_details.dart
@@ -13,7 +13,7 @@ class BrowseDetails extends StatefulWidget {
   BrowseDetails({required this.itemView});
 
   @override
-  State<BrowseDetails> createState() => _BrowseDetailsState();
+  _BrowseDetailsState createState() => _BrowseDetailsState();
 }
 
 class _BrowseDetailsState extends State<BrowseDetails> {
@@ -39,6 +39,7 @@ class _BrowseDetailsState extends State<BrowseDetails> {
         isLoading = false;
       });
     } catch (e) {
+      // Handle error, e.g., display an error message
       print(e);
     }
   }
@@ -55,86 +56,93 @@ class _BrowseDetailsState extends State<BrowseDetails> {
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            movies.isEmpty
-                ? Center(
-                    child: Text(
-                      'No movies available',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                      ),
-                    ),
-                  )
-                : CarouselSlider.builder(
-                    itemCount: movies.length,
-                    options: CarouselOptions(
-                      height: MediaQuery.of(context).size.height * 0.30,
-                      // Adjust height as needed
-                      autoPlay: true,
-                      viewportFraction: 0.55,
-                      enlargeCenterPage: true,
-                      pageSnapping: true,
-                      autoPlayCurve: Curves.fastOutSlowIn,
-                      autoPlayAnimationDuration: const Duration(seconds: 1),
-                    ),
-                    itemBuilder:
-                        (BuildContext context, int index, int realIndex) {
-                      final movie = movies[index];
-                      return InkWell(
-                        onTap: () {
-                          Navigator.pushNamed(context, MovieDetails.routeName,
-                              arguments: movie);
-                        },
-                        child: Container(
-                          child: Stack(
-                            children: [
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(10.0),
-                                child: CachedNetworkImage(
-                                  imageUrl:
-                                      '${ApiConstant.imageBaseUrl}${movie.backdropPath ?? ""}',
-                                  imageBuilder: (context, imageProvider) =>
-                                      Container(
-                                    width: double.infinity,
-                                    decoration: BoxDecoration(
-                                      image: DecorationImage(
-                                        image: imageProvider,
-                                        fit: BoxFit.cover,
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  movies.isEmpty
+                      ? Center(
+                          child: Text(
+                            'No movies available',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                        )
+                      : CarouselSlider.builder(
+                          itemCount: movies.length,
+                          options: CarouselOptions(
+                            height: MediaQuery.of(context).size.height * 0.30,
+                            // Adjust height as needed
+                            autoPlay: true,
+                            viewportFraction: 0.55,
+                            enlargeCenterPage: true,
+                            pageSnapping: true,
+                            autoPlayCurve: Curves.fastOutSlowIn,
+                            autoPlayAnimationDuration:
+                                const Duration(seconds: 1),
+                          ),
+                          itemBuilder:
+                              (BuildContext context, int index, int realIndex) {
+                            final movie = movies[index];
+                            return InkWell(
+                              onTap: () {
+                                Navigator.pushNamed(
+                                    context, MovieDetails.routeName,
+                                    arguments: movie);
+                              },
+                              child: Container(
+                                child: Stack(
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      child: CachedNetworkImage(
+                                        imageUrl:
+                                            '${ApiConstant.imageBaseUrl}${movie.backdropPath ?? ""}',
+                                        imageBuilder:
+                                            (context, imageProvider) =>
+                                                Container(
+                                          width: double.infinity,
+                                          decoration: BoxDecoration(
+                                            image: DecorationImage(
+                                              image: imageProvider,
+                                              fit: BoxFit.cover,
+                                            ),
+                                          ),
+                                        ),
+                                        placeholder: (context, url) =>
+                                            CircularProgressIndicator(),
+                                        errorWidget: (context, url, error) =>
+                                            Center(
+                                          child: Icon(Icons.error,
+                                              color: Colors.red, size: 42),
+                                        ),
                                       ),
                                     ),
-                                  ),
-                                  placeholder: (context, url) =>
-                                      CircularProgressIndicator(),
-                                  errorWidget: (context, url, error) => Center(
-                                    child: Icon(Icons.error,
-                                        color: Colors.red, size: 42),
-                                  ),
+                                    Center(
+                                      child:
+                                          Image.asset('assets/playbutton.png'),
+                                    ),
+                                  ],
                                 ),
                               ),
-                              Center(
-                                child: Image.asset('assets/playbutton.png'),
-                              ),
-                            ],
-                          ),
+                            );
+                          },
                         ),
-                      );
-                    },
-                  ),
-            SizedBox(height: 20),
+                  SizedBox(height: 20),
 
-            // Add the Text widget here
-            Container(
-              color: Theme.of(context).colorScheme.background,
-              child: Text(
-                'Your additional text goes here',
-                // Add your desired text content
-                style: TextStyle(
-                  fontSize: 20, // Adjust the font size
-                  fontWeight: FontWeight.bold, // Customize the style
+                  // Add the Text widget here
+                  Container(
+                    color: Theme.of(context).colorScheme.background,
+                    child: Text(
+                      'Your additional text goes here',
+                      // Add your desired text content
+                      style: TextStyle(
+                        fontSize: 20, // Adjust the font size
+                        fontWeight: FontWeight.bold, // Customize the style
                   color: Colors.white, // Customize the text color
                 ),
               ),
@@ -156,13 +164,13 @@ class _BrowseDetailsState extends State<BrowseDetails> {
                   CarouselSlider.builder(
                     itemCount: movies.length,
                     options: CarouselOptions(
-                      enableInfiniteScroll: true,
-                      disableCenter: false,
-                      height: MediaQuery.of(context).size.height *
-                          0.4, // Adjust the height as needed
-                      viewportFraction:
-                          0.6, // Adjust the fraction to fit the content
-                    ),
+                            enableInfiniteScroll: true,
+                            disableCenter: false,
+                            height: MediaQuery.of(context).size.height *
+                                0.4, // Adjust the height as needed
+                            viewportFraction:
+                                0.6, // Adjust the fraction to fit the content
+                          ),
                     itemBuilder:
                         (BuildContext context, int index, int realIndex) {
                       final movie = movies[index];
@@ -175,42 +183,44 @@ class _BrowseDetailsState extends State<BrowseDetails> {
                               Expanded(
                                 flex: 5,
                                 child: Container(
-                                  padding: EdgeInsets.symmetric(horizontal: 5),
-                                  child: Stack(
-                                    children: [
-                                      InkWell(
-                                        onTap: () {
-                                          Navigator.pushNamed(
-                                              context, MovieDetails.routeName,
-                                              arguments: movie);
-                                        },
-                                        child: CachedNetworkImage(
-                                          imageUrl:
-                                              "${ApiConstant.imageBaseUrl}${movie.backdropPath ?? ""}",
-                                          imageBuilder:
-                                              (context, imageProvider) =>
-                                                  Container(
-                                            height: MediaQuery.of(context)
-                                                    .size
-                                                    .height *
-                                                0.4,
-                                            // Adjust the height as needed
-                                            width: double.infinity,
-                                            decoration: BoxDecoration(
-                                              image: DecorationImage(
-                                                image: imageProvider,
-                                                fit: BoxFit.cover,
+                                        padding:
+                                            EdgeInsets.symmetric(horizontal: 5),
+                                        child: Stack(
+                                          children: [
+                                            InkWell(
+                                              onTap: () {
+                                                Navigator.pushNamed(context,
+                                                    MovieDetails.routeName,
+                                                    arguments: movie);
+                                              },
+                                              child: CachedNetworkImage(
+                                                imageUrl:
+                                                    "${ApiConstant.imageBaseUrl}${movie.backdropPath ?? ""}",
+                                                imageBuilder:
+                                                    (context, imageProvider) =>
+                                                        Container(
+                                                  height: MediaQuery.of(context)
+                                                          .size
+                                                          .height *
+                                                      0.4,
+                                                  // Adjust the height as needed
+                                                  width: double.infinity,
+                                                  decoration: BoxDecoration(
+                                                    image: DecorationImage(
+                                                      image: imageProvider,
+                                                      fit: BoxFit.cover,
+                                                    ),
+                                                  ),
+                                                ),
+                                                placeholder: (context, url) =>
+                                                    CircularProgressIndicator(),
+                                                errorWidget: (context, url,
+                                                        error) =>
+                                                    Center(
+                                                        child: Icon(Icons.error,
+                                                            color: Colors.red,
+                                                            size: 42)),
                                               ),
-                                            ),
-                                          ),
-                                          placeholder: (context, url) =>
-                                              CircularProgressIndicator(),
-                                          errorWidget: (context, url, error) =>
-                                              Center(
-                                                  child: Icon(Icons.error,
-                                                      color: Colors.red,
-                                                      size: 42)),
-                                        ),
                                       ),
                                     ],
                                   ),
